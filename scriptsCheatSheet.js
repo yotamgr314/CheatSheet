f2edab01affffe03573703d6e8cfe6539c5783b9
Yotam Green <yotamgr314@gmail.com>
2024-05-28 20:04:25 +0300

    Update scriptsCheatSheet.js
    asdasdasdasdd

--- scriptsCheatSheet.js
+++ scriptsCheatSheet.js
@@ -26,26 +26,45 @@ const person = {
 
 
 /* JAVASCRIPT HOISTING */
-1) Function Declarations: The entire function body is hoisted to the top of the scope, so you can call them before they appear in the code.
-2) Function Expressions: Only the declaration of the variable is hoisted to the top of the scope, but not the assignment itself. 
-3) Therefore, if you try to call the function before the assignment, you will get a TypeError because the variable is undefined.
-5) Understanding this difference is important when writing and understanding JavaScript code, especially when working with functions and the impact of hoisting.
+
 1) Hoisting is the process where variable and function declarations are moved to the top of their scope.
-2) var: Declarations are hoisted, but values are not. If you access the variable before the assignment, you will get undefined.
-3) let and const: Declarations are hoisted but enter the Temporal Dead Zone (TDZ). Accessing the variable before the declaration will cause a ReferenceError.
-4) Functions: Function declarations are fully hoisted, so you can call them before they appear in the code. In contrast, function expressions are not fully hoisted,
+
+2) Function Declarations: The entire function body is hoisted to the top of the scope, so you can call them before they appear in the code.
+
+3) Function Expressions: Only the declaration of the variable is hoisted to the top of the scope, but not the assignment itself. 
+   Therefore, if you try to call the function before the assignment, you will get a TypeError because the variable is undefined.
+
+4) var: Declarations are hoisted, but values are not. If you access the variable before the assignment, you will get undefinederror.
+
+5) let and const: Declarations are hoisted but enter the Temporal Dead Zone (TDZ). Accessing the variable before the declaration will cause a ReferenceError.
+
+6) Functions: Function declarations are fully hoisted, so you can call them before they appear in the code. In contrast, function expressions are not fully hoisted,
    and accessing them before the assignment will cause a TypeError.
-   This is an important feature to know and understand how it affects the order in which JavaScript code is executed.
+
+
+
+/* IIFE - IMMIDETALY INVOKED FUNCTION */
+
+let a = 8; 
+let b = 2; 
+(function () {
+a = 10; 
+b = 5; 
+console.log("a+b = ", a +b); 
+} )();
+
+console.log("a = ", a); 
+console.log("b = ", b); 
+
+
+/* a and b Declared outside of any function or block --> hence we can access them and modify them within any function in our code.
+our function is an IIFE, which means it is invoked immediately after it is defined.
+*/
 
 
 
 
-
-
-
-
-
-
+/* THIS  */
 
 
 
